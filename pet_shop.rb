2bf309b337aa24ce0596862771a9f439e3182895
mjpayne8 <markpayne@Marks-MacBook-Air.local>
2019-09-13 11:53:22 +0100

    function 5  written

--- pet_shop.rb
+++ pet_shop.rb
@@ -13,3 +13,7 @@ end
 def pets_sold(pet_shop)
   return pet_shop[:admin][:pets_sold]
 end
+
+def increase_pets_sold(pet_shop, number_of_pets_sold)
+  pet_shop[:admin][:pets_sold] += number_of_pets_sold
+end
