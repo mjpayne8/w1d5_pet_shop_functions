af43b0e6c6751a22cc9f64ced4a043d116027d96
mjpayne8 <markpayne@Marks-MacBook-Air.local>
2019-09-13 11:55:19 +0100

    function 6  written

--- pet_shop.rb
+++ pet_shop.rb
@@ -17,3 +17,7 @@ end
 def increase_pets_sold(pet_shop, number_of_pets_sold)
   pet_shop[:admin][:pets_sold] += number_of_pets_sold
 end
+
+def stock_count(pet_shop)
+  return pet_shop[:pets].length()
+end
